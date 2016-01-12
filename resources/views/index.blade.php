cb4e9885a40b086ad8384fc91ece0942e51865c9
jhserodio <jhserodio@gmail.com.br>
2016-01-11 23:43:31 -0200

    style bases - grid-setting and forms

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,7 @@
 
 <section>
 
-  <button type="button" name="button">botao</button>
-  <input type="button" name="name" value="botao">
-  <input type="submit" name="name" value="botao">
-  <a href="#" class="btn">Botao</a>
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
 </section>
 
