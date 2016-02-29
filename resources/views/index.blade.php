3427fb459d1df3df5e661bac1ee7c24601f8406b
jhserodio <jhserodio@gmail.com>
2016-02-28 21:41:15 -0300

    style home clear

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,196 @@
 @extends ('template')
 @section('content')
 
-<section>
+<section class="so-home">
 
-<i class="icon-home"></i>
+  <div class="banner">
+    <div class="card-triple">
+      <div class="card">
+        <article class="banner-item">
+          <header>
+            <h1 class="title-banner">
+              Titulo
+            </h1>
+          </header>
+          <main>
+            <img src="#" alt="banner image" />
+          </main>
+          <footer>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+            </p>
+          </footer>
+        </article>
+      </div>
 
-<i class="icon-build"></i>
+      <div class="card">
+        <article class="banner-item">
+          <header>
+            <h1 class="title-banner">
+              Exemplo
+            </h1>
+          </header>
+          <main>
+            <img src="#" alt="banner image" />
+          </main>
+          <footer>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            </p>
+          </footer>
+        </article>
+      </div>
+
+      <div class="card">
+        <article class="banner-item">
+          <header>
+            <h1 class="title-banner">
+              titulo
+            </h1>
+          </header>
+          <main>
+            <img src="#" alt="banner image" />
+          </main>
+          <footer>
+            <p>
+              Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </footer>
+        </article>
+      </div>
+    </div>
+  </div>
+
+  <div class="home-info">
+    <div class="card-double">
+      <div class="card">
+          <h2 class="title">Insitucional</h2>
+          <p>
+            <i class="icon-build"></i>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Dolore minima itaque voluptatem corporis non molestiae eaque,
+            accusantium quasi dignissimos vel quas, harum aperiam eveniet,
+            reiciendis aspernatur incidunt eligendi inventore accusamus?
+          </p>
+          <a href="#">
+            <button class="btn-link">
+              ler mais
+            </button>
+          </a>
+      </div>
+
+      <div class="card">
+          <h2 class="title">Cartão Oeste</h2>
+          <p>
+            <i class="icon-card"></i>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Dolore minima itaque voluptatem corporis non molestiae eaque,
+            accusantium quasi dignissimos vel quas, harum aperiam eveniet,
+            reiciendis aspernatur incidunt eligendi inventore accusamus?
+          </p>
+          <a href="#">
+            <button class="btn-link">
+              peça o seu
+            </button>
+          </a>
+      </div>
+
+    </div>
+  </div>
+
+  <div class="home-news">
+    <h2 class="title">Notícias</h2>
+    <div class="card-four">
+
+      <div class="card">
+        <article class="news-thumb">
+          <header class="nt-header">
+            <h3 class="title-news">Titulo da noticia</h3>
+            <time> 00:00 - 12 de setembro de 2012</time>
+          </header>
+          <main class="nt-main">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium asperiores aperiam,
+              perspiciatis exercitationem nihil aspernatur iste nam quis eaque perferendis
+              unde sit a doloribus molestias cupiditate id ad maxime accusantium?
+            </p>
+          </main>
+          <footer class="nt-footer">
+            <a href="#">
+              <button class="btn-link">Ler Mais</button>
+            </a>
+          </footer>
+        </article>
+      </div>
+
+      <div class="card">
+        <article class="news-thumb">
+          <header class="nt-header">
+            <h3 class="title-news">Titulo da noticia</h3>
+            <time> 00:00 - 12 de setembro de 2012</time>
+          </header>
+          <main class="nt-main">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium asperiores aperiam,
+              perspiciatis exercitationem nihil aspernatur iste nam quis eaque perferendis
+              unde sit a doloribus molestias cupiditate id ad maxime accusantium?
+            </p>
+          </main>
+          <footer class="nt-footer">
+            <a href="#">
+              <button class="btn-link">Ler Mais</button>
+            </a>
+          </footer>
+        </article>
+      </div>
+
+      <div class="card">
+        <article class="news-thumb">
+          <header class="nt-header">
+            <h3 class="title-news">Titulo da noticia</h3>
+            <time> 00:00 - 12 de setembro de 2012</time>
+          </header>
+          <main class="nt-main">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium asperiores aperiam,
+              perspiciatis exercitationem nihil aspernatur iste nam quis eaque perferendis
+              unde sit a doloribus molestias cupiditate id ad maxime accusantium?
+            </p>
+          </main>
+          <footer class="nt-footer">
+            <a href="#">
+              <button class="btn-link">Ler Mais</button>
+            </a>
+          </footer>
+        </article>
+      </div>
+
+      <div class="card">
+        <article class="news-thumb">
+          <header class="nt-header">
+            <h3 class="title-news">Titulo da noticia</h3>
+            <time> 00:00 - 12 de setembro de 2012</time>
+          </header>
+          <main class="nt-main">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium asperiores aperiam,
+              perspiciatis exercitationem nihil aspernatur iste nam quis eaque perferendis
+              unde sit a doloribus molestias cupiditate id ad maxime accusantium?
+            </p>
+          </main>
+          <footer class="nt-footer">
+            <a href="#">
+              <button class="btn-link">Ler Mais</button>
+            </a>
+          </footer>
+        </article>
+      </div>
+
+    </div>
+  </div>
 
 </section>
-
 
 @endsection
